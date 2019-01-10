63a47b2a4945f3e186f4da80becad864f7636950
matteo francesconi <matteo.francesconi@davide.it>
2019-01-10 19:12:44 +0100

    ended

--- src/main/java/com/mattymatty/RestApi.java
+++ src/main/java/com/mattymatty/RestApi.java
@@ -2,10 +2,11 @@ package com.mattymatty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class RestApi {
+public class RestApi extends SpringBootServletInitializer {
     static ServerController controller;
     public static void main(String[] arguments){
         new Thread((controller = new ServerController(23446,"rolegroup"))).start();
